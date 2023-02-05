9dc85e805d214e1f92aa5a54c3f5fdd749c2ba7d
Maria Dikul <dikul.m@mail.ru>
2023-02-05 14:35:57 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        OnTaskDoneListener listener = System.out::println;
+        OnTaskErrorListener errorListener = System.out::println;
+        Worker worker = new Worker(listener, errorListener);
+        worker.start();
+    }
+}
---
+++ src/Worker.java
@@ -0,0 +1,19 @@
+public class Worker {
+    private OnTaskDoneListener callback;
+    private OnTaskErrorListener errorCallback;
+
+    public Worker(OnTaskDoneListener callback, OnTaskErrorListener errorCallback) {
+        this.callback = callback;
+        this.errorCallback = errorCallback;
+    }
+
+    public void start() {
+        for (int i = 0; i < 100; i++) {
+            if (i == 33) {
+                errorCallback.onError("Task " + i + " is error");
+            } else {
+                callback.onDone("Task " + i + " is done");
+            }
+        }
+    }
+}
